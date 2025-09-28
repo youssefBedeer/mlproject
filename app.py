1e23d6d7056d9dbdabb4d563525ec256ac371b67
Youssef Bedeer <yousser1bedeer@gmail.com>
2025-09-28 22:59:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np 
 import pandas as pd
-
+#
 from sklearn.preprocessing import StandardScaler 
 from src.pipeline.prediction_pipeline import CustomeData, PredictPipeline
 
